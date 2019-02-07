5a5cfac3337eb9ec34924d151c47f9c8243bbe4a
DaliIsNoMore <47418886+DaliIsNoMore@users.noreply.github.com>
2019-02-07 23:31:05 +0100

    Add files via upload

---
+++ LeVeloCestCool/src/Source/Gestionnaire.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Source;
+
+import java.util.Date;
+
+/**
+ *
+ * @author GREGO
+ */
+public class Gestionnaire extends MembreSociete{
+    
+    public Gestionnaire (String lg, String m, String p, String n, Date de)
+    {
+        super(lg,m,p,n,de);
+    }
+}

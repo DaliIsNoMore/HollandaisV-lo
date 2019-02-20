9a4f68939b830ecd648edf3bdf2c598bfd1f4fdf
DaliIsNoMore <47418886+DaliIsNoMore@users.noreply.github.com>
2019-02-20 15:09:43 +0100

    Add files via upload

---
+++ LeVeloCestCool/src/Utilisation/GestionAbonne.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilisation;
+
+import Source.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author GREGO
+ */
+public class GestionAbonne {
+    private ArrayList<Abonne>listeAbonne;
+    private ArrayList<Location>listeLocation;
+}
---
+++ LeVeloCestCool/src/Utilisation/GestionTechnicien.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilisation;
+import Source.*;
+import java.util.ArrayList;
+/**
+ *
+ * @author GREGO
+ */
+public class GestionTechnicien {
+    private ArrayList<FicheReparation>listeFicheReparation;
+    private ArrayList<Velo>listeVelo;
+    private ArrayList<Borne>listeBorne;
+}
